1f58609fb18c498e975cd542a182069e64f2bd27
Chittaranjan Sardar <crsardar@gmail.com>
2020-11-20 12:00:53 +0530

    Hands On React - Handling User Input with Forms and Events, Showing and Playing Videos

--- videos/src/api/youtube.js
+++ videos/src/api/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyDurk6fiQbak9sBYYXOuricYfJ4wAk4iqM";
+const KEY = "MY-KEY";
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
